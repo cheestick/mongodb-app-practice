ba062623ae5dd3a9382305c6dde6d82f46b35a95
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-08-07 22:18:19 +0300

    validation joi

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
-const { ctrlWrapper } = require('../../middlewares/')
+const { ctrlWrapper, validation } = require('../../middlewares/')
 const { products: ctrl } = require('../../controllers')
+const { joiSchema } = require('../../models/product')
 
 router.get('/', ctrlWrapper(ctrl.getAll))
-router.post('/', ctrlWrapper(ctrl.add))
+// router.get('/:id', ctrlWrapper(ctrl.getById))
+router.post('/', validation(joiSchema), ctrlWrapper(ctrl.add))
+// router.put('/:id', validation(joiSchema), ctrlWrapper(ctrl.updateById))
+// router.delete('/:id', ctrlWrapper(ctrl.removeById))
 
 module.exports = router
