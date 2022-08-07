d60bcc5d073e4a3950878b55a3389d3e74b90119
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-08-07 13:16:26 +0300

    error mongodb connection processed. sctipt added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 //maksym m1a2k3s4y5m6
 
+const mongoose = require('mongoose')
+
 const DB_HOST =
-  "mongodb+srv://maksym:m1a2k3s4y5m6@cluster0.enlhisl.mongodb.net/online_shop?retryWrites=true&w=majority";
+  'mongodb+srv://maksym:m1a2k3s4y5m6@cluster0.enlhisl.mongodb.net/online_shop?retryWrites=true&w=majority'
 
-const mongoose = require("mongoose");
-
-mongoose.connect();
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log('Database connected'))
+  .catch((e) => {
+    console.log(e.message)
+    process.exit(1)
+  })
