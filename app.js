d5a9683b7ce2e93bad07f2c0de0257b858a2c6e0
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-08-07 13:09:53 +0300

    mongodb plus mongoose app inited

---
+++ app.js
@@ -0,0 +1,8 @@
+//maksym m1a2k3s4y5m6
+
+const DB_HOST =
+  "mongodb+srv://maksym:m1a2k3s4y5m6@cluster0.enlhisl.mongodb.net/online_shop?retryWrites=true&w=majority";
+
+const mongoose = require("mongoose");
+
+mongoose.connect();
